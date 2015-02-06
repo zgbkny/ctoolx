af2c5d1ba8066071f04391f3e4250ce4fa67ef41
zgbkny <zgbkny@gmail.com>
2015-02-06 10:57:36 +0800

    fork

---
+++ src/ctoolx_math/math.h
@@ -0,0 +1,14 @@
+#ifndef CTOOLX_MATH_MATH_H
+#define CTOOLX_MATH_MATH_H
+
+#ifndef __cplusplus
+//extern "C" {
+#endif
+#include <stdlib.h>
+
+size_t fibonacci(size_t n);
+
+#ifndef __cplusplus
+//}
+#endif
+#endif
--- src/test.c
+++ src/test.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <ctoolx_math/math.h>
+
+#include <netdb.h>
+#include <sys/socket.h>
+
+#include <unistd.h>
+#include <sys/types.h>
 
 int main() 
 {
+
+	
 	int i = 10;
-	float f = i / 3;
-	printf("hello:%f\n", f);
+	i = fibonacci(10);
+	printf("hello:%d\n", i);
+
+	i = 0;
+	for ( ; i < 2; i++) {
+		fork();
+		printf("-\n");
+	}
+
 	return 0;
 }
